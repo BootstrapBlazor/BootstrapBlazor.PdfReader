2f8af09ecdc3212d63c327f2aad14ee18cb4cf51
Alex Chow <zhouchuanglin@gmail.com>
2023-01-02 16:05:46 +0100

    添加参数 ReadOnly 禁用复制/打印/下载, 参数 Watermark 水印内容
    添加参数 ReadOnly 禁用复制/打印/下载
    添加参数 Watermark 水印内容
    EnumZoomMode 更新
    Filename参数改名FileName

---
+++ src/BootstrapBlazor.PdfReader/wwwroot/web/tools.js
@@ -0,0 +1,19 @@
+﻿//屏蔽功能按钮
+function onBodyLoad() {
+
+    if (getQueryVariable("wm"))
+    { 
+        document.getElementById("watermark").value = getQueryVariable("wm");
+    }
+}
+
+function getQueryVariable(variable) {
+    console.log(variable, window.location)
+    var query = window.location.hash.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) { return pair[1]; }
+    }
+    return (false);
+}
